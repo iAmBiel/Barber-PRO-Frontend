cf5533b947ddd9f5b2b64a631753112cdac2a42f
iAmBiel <bielvm02@gmail.com>
2024-03-05 19:10:01 -0300

    frontend init

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import { Flex, Text } from '@chakra-ui/react';
+
+import { canSSRAuth } from '../../utils/canSSRAuth'
+
+export default function Dashboard(){
+  return(
+    <>
+      <Head>
+        <title>BarberPRO - Minha barbearia</title>
+      </Head>
+      <Flex>
+        <Text>Bem vindo ao dashboard</Text>
+      </Flex>
+    </>
+  )
+}
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  return {
+    props: {}
+  }
+})
